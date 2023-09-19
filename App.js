ec0ac974a6773939d15da0e9a896649869f7b621
Leonardo Sebastian Gauto <112297703+LeoSebastian23@users.noreply.github.com>
2023-09-18 22:56:07 -0300

    Boton
    Se agrega botón

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
+//import React, {useState} from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
 import image01 from './assets/android-image.png';
 
 export default function App() {
+ 
   return (
+    
     <View style={styles.container}>
       <Text style={styles.title}>Hola mundo!</Text>
       <Image style= {styles.image}
       source= {image01} 
       />
+       <Button
+        title="Presiona el boton"
+        onPress={() => Alert.alert('Bienvenido a la aplicación!')}
+      />
+      
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
